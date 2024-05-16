2d250e381d4e02e915633b340f4670281b663e30
Andrey <rusefillc@gmail.com>
2024-05-15 23:47:12 -0400

    extra manual fix

--- extra_generated.h
+++ extra_generated.h
@@ -1,10 +1,10 @@
-// this section was generated automatically by rusEFI tool config_definition_base-all.jar based on (unknown script) ../../../extra.txt Wed May 15 21:50:20 EDT 2024
+// this section was generated automatically by rusEFI tool config_definition_base-all.jar based on (unknown script) ../../../extra.txt Wed May 15 23:34:33 EDT 2024
 // by class com.rusefi.output.CHeaderConsumer
 // begin
 #pragma once
 #include "rusefi_types.h"
-// start of board_extra_s
-struct board_extra_s {
+// start of extra_s
+struct extra_s {
 	/**
 	 * offset 0
 	 */
@@ -14,7 +14,7 @@ struct board_extra_s {
 	 */
 	uint8_t unusedAtTheExtraEnd[92];
 };
-static_assert(sizeof(board_extra_s) == 96);
+static_assert(sizeof(extra_s) == 96);
 
 // end
-// this section was generated automatically by rusEFI tool config_definition_base-all.jar based on (unknown script) ../../../extra.txt Wed May 15 21:50:20 EDT 2024
+// this section was generated automatically by rusEFI tool config_definition_base-all.jar based on (unknown script) ../../../extra.txt Wed May 15 23:34:33 EDT 2024
