8fbf4771da800b4ee8f8b8cad0aefaf4097ce79b
GitHub git update Action <action@github.com>
2024-06-16 16:43:09 +0000

    Auto-generated Configs
    [no ci]

--- generated/controllers/generated/engine_configuration_generated_structures_XYZ.h
+++ generated/controllers/generated/engine_configuration_generated_structures_XYZ.h
@@ -1,4 +1,4 @@
-// this section was generated automatically by rusEFI tool config_definition-all.jar based on (unknown script) integration/rusefi_config.txt Mon Jun 10 16:10:48 UTC 2024
+// this section was generated automatically by rusEFI tool config_definition-all.jar based on (unknown script) integration/rusefi_config.txt Sun Jun 16 16:40:26 UTC 2024
 // by class com.rusefi.output.CHeaderConsumer
 // begin
 #pragma once
@@ -2175,7 +2175,7 @@ struct engine_configuration_s {
 	/**
 	 * offset 992
 	 */
-	int unusedHere13;
+	float mapExpAverageAlpha;
 	/**
 	 * offset 996
 	 */
@@ -5641,4 +5641,4 @@ struct persistent_config_s {
 static_assert(sizeof(persistent_config_s) == 22752);
 
 // end
-// this section was generated automatically by rusEFI tool config_definition-all.jar based on (unknown script) integration/rusefi_config.txt Mon Jun 10 16:10:48 UTC 2024
+// this section was generated automatically by rusEFI tool config_definition-all.jar based on (unknown script) integration/rusefi_config.txt Sun Jun 16 16:40:26 UTC 2024
--- generated/controllers/generated/signature_XYZ.h
+++ generated/controllers/generated/signature_XYZ.h
@@ -2,5 +2,5 @@
 // was generated automatically by rusEFI tool config_definition-all.jar based on gen_config.sh by SignatureConsumer
 //
 
-#define SIGNATURE_HASH 2520315447
-#define TS_SIGNATURE "rusEFI main.2024.06.15.XYZ.2520315447"
+#define SIGNATURE_HASH 2509251213
+#define TS_SIGNATURE "rusEFI main.2024.06.16.XYZ.2509251213"
