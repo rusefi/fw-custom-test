7944133a5d43991597a165f4ecbf3ede057a47f5
GitHub git update Action <action@github.com>
2024-09-02 04:31:44 +0000

    Custom Auto-generated Configs
    [no ci]

---
+++ generated/extra_generated.h
@@ -0,0 +1,24 @@
+// this section was generated automatically by rusEFI tool config_definition_base-all.jar based on (unknown script) ../../../extra.txt
+// by class com.rusefi.output.CHeaderConsumer
+// begin
+#pragma once
+#include "rusefi_types.h"
+// start of extra_s
+struct extra_s {
+	/**
+	 * offset 0
+	 */
+	int extraField = (int)0;
+	/**
+	 * offset 4
+	 */
+	int extraCustomGauge = (int)0;
+	/**
+	 * offset 8
+	 */
+	uint8_t unusedAtTheExtraEnd[92];
+};
+static_assert(sizeof(extra_s) == 100);
+
+// end
+// this section was generated automatically by rusEFI tool config_definition_base-all.jar based on (unknown script) ../../../extra.txt
