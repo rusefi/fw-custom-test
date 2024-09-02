b034efa75f6d0dada9026368f10b93f013c43515
Andrey <rusefillc@gmail.com>
2024-09-01 23:30:19 -0400

    https://github.com/rusefi/fw-custom-example/issues/14

--- compile_firmware.sh
+++ compile_firmware.sh
@@ -4,5 +4,6 @@ export EXTRA_LIVE_DATA_FILE=",../../../board_extra.yaml"
 # todo: code reuse with manual-generate-meta-data.sh?
 export META_OUTPUT_ROOT_FOLDER="../../../generated/"
 
+export AUTOMATION_REF=`git branch --show-current`
 cd ext/rusefi/firmware/
 bash bin/compile.sh ../../../meta-info.env
