1dbcc34d41e0af5830ee7351ad244e40afa7a3fb
Andrey <rusefillc@gmail.com>
2024-09-01 23:11:24 -0400

    https://github.com/rusefi/fw-custom-example/issues/14

--- compile_firmware.sh
+++ compile_firmware.sh
@@ -1,5 +1,8 @@
 #!/usr/bin/env bash
 
-# do we need export EXTRA_LIVE_DATA_FILE=",../../../board_extra.yaml" here?
+export EXTRA_LIVE_DATA_FILE=",../../../board_extra.yaml"
+# todo: code reuse with manual-generate-meta-data.sh?
+export META_OUTPUT_ROOT_FOLDER="../../../generated/"
+
 cd ext/rusefi/firmware/
 bash bin/compile.sh ../../../meta-info.env
