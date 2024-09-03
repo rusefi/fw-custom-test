93feb31b97315f50b4965a8fbdf312e38c6c1874
Andrey <rusefillc@gmail.com>
2024-09-02 23:59:31 -0400

    improving

--- compile_firmware.sh
+++ compile_firmware.sh
@@ -2,7 +2,8 @@
 
 export EXTRA_LIVE_DATA_FILE=",../../../board_extra.yaml"
 # todo: code reuse with manual-generate-meta-data.sh?
-export META_OUTPUT_ROOT_FOLDER="../../../generated/"
+export META_OUTPUT_ROOT_FOLDER=$(realpath --relative-to=ext/rusefi/firmware generated)/
+echo "META_OUTPUT_ROOT_FOLDER=$META_OUTPUT_ROOT_FOLDER"
 
 export AUTOMATION_REF=`git branch --show-current`
 cd ext/rusefi/firmware/
