0aba4583f42698255d55a1d19890cacf582fcb17
GitHub git update Action <action@github.com>
2024-09-06 01:18:33 +0000

    Custom Auto-generated Configs
    [no ci]

--- generated/controllers/generated/signature_XYZ.h
+++ generated/controllers/generated/signature_XYZ.h
@@ -2,5 +2,5 @@
 // was generated automatically by rusEFI tool config_definition-all.jar based on gen_config.sh by SignatureConsumer
 //
 
-#define SIGNATURE_HASH 3644004984
-#define TS_SIGNATURE "rusEFI main.2024.09.05.XYZ.3644004984"
+#define SIGNATURE_HASH 1434061329
+#define TS_SIGNATURE "rusEFI main.2024.09.06.XYZ.1434061329"
---
+++ generated/live_data_generated/extra_generated.h
@@ -0,0 +1,120 @@
+// this section was generated automatically by rusEFI tool config_definition_base-all.jar based on (unknown script) ../../../extra.txt
+// by class com.rusefi.output.CHeaderConsumer
+// begin
+#pragma once
+#include "rusefi_types.h"
+// start of extra_s
+struct extra_s {
+	/**
+	 * offset 0
+	 */
+	int extraField = (int)0;
+	/**
+	 * offset 4
+	 */
+	int extraCustomGauge = (int)0;
+	/**
+	offset 8 bit 0 */
+	bool customBit : 1 {};
+	/**
+	offset 8 bit 1 */
+	bool unusedBit_3_1 : 1 {};
+	/**
+	offset 8 bit 2 */
+	bool unusedBit_3_2 : 1 {};
+	/**
+	offset 8 bit 3 */
+	bool unusedBit_3_3 : 1 {};
+	/**
+	offset 8 bit 4 */
+	bool unusedBit_3_4 : 1 {};
+	/**
+	offset 8 bit 5 */
+	bool unusedBit_3_5 : 1 {};
+	/**
+	offset 8 bit 6 */
+	bool unusedBit_3_6 : 1 {};
+	/**
+	offset 8 bit 7 */
+	bool unusedBit_3_7 : 1 {};
+	/**
+	offset 8 bit 8 */
+	bool unusedBit_3_8 : 1 {};
+	/**
+	offset 8 bit 9 */
+	bool unusedBit_3_9 : 1 {};
+	/**
+	offset 8 bit 10 */
+	bool unusedBit_3_10 : 1 {};
+	/**
+	offset 8 bit 11 */
+	bool unusedBit_3_11 : 1 {};
+	/**
+	offset 8 bit 12 */
+	bool unusedBit_3_12 : 1 {};
+	/**
+	offset 8 bit 13 */
+	bool unusedBit_3_13 : 1 {};
+	/**
+	offset 8 bit 14 */
+	bool unusedBit_3_14 : 1 {};
+	/**
+	offset 8 bit 15 */
+	bool unusedBit_3_15 : 1 {};
+	/**
+	offset 8 bit 16 */
+	bool unusedBit_3_16 : 1 {};
+	/**
+	offset 8 bit 17 */
+	bool unusedBit_3_17 : 1 {};
+	/**
+	offset 8 bit 18 */
+	bool unusedBit_3_18 : 1 {};
+	/**
+	offset 8 bit 19 */
+	bool unusedBit_3_19 : 1 {};
+	/**
+	offset 8 bit 20 */
+	bool unusedBit_3_20 : 1 {};
+	/**
+	offset 8 bit 21 */
+	bool unusedBit_3_21 : 1 {};
+	/**
+	offset 8 bit 22 */
+	bool unusedBit_3_22 : 1 {};
+	/**
+	offset 8 bit 23 */
+	bool unusedBit_3_23 : 1 {};
+	/**
+	offset 8 bit 24 */
+	bool unusedBit_3_24 : 1 {};
+	/**
+	offset 8 bit 25 */
+	bool unusedBit_3_25 : 1 {};
+	/**
+	offset 8 bit 26 */
+	bool unusedBit_3_26 : 1 {};
+	/**
+	offset 8 bit 27 */
+	bool unusedBit_3_27 : 1 {};
+	/**
+	offset 8 bit 28 */
+	bool unusedBit_3_28 : 1 {};
+	/**
+	offset 8 bit 29 */
+	bool unusedBit_3_29 : 1 {};
+	/**
+	offset 8 bit 30 */
+	bool unusedBit_3_30 : 1 {};
+	/**
+	offset 8 bit 31 */
+	bool unusedBit_3_31 : 1 {};
+	/**
+	 * offset 12
+	 */
+	uint8_t unusedAtTheExtraEnd[92];
+};
+static_assert(sizeof(extra_s) == 104);
+
+// end
+// this section was generated automatically by rusEFI tool config_definition_base-all.jar based on (unknown script) ../../../extra.txt
